04874d68cba97aa886f1408f31f8f2024c7d71bc
nerdropy <yusuft.5097@gmail.com>
2024-02-16 19:25:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,31 @@ class Library:
         print("Destructor called, Library deleted.")
 
     def listBooks(self):
-        pass
+        txt = self.lib.read().splitlines()
+        for i in txt:
+            print(i)
+
+    def addBook(self):
+        # self.title = title
+        # self.author = author
+        # self.year = year
+        # self.pages = pages
+
+        metadata = [x for x in input('Add the book information (title, author, year, pages):').split(', ')]
+        wcomma = ', '.join(metadata)
+        self.lib.write(f"{wcomma}\n")
+        print(self.lib)
+
+
+
+obj1 = Library()
+obj2 = Library()
+obj1.addBook()
+obj2.addBook()
+
+print(obj1)
+        
+
 
 
 
