a4aee5e837ff437db57f727dabd9c9e17d802ffc
nerdropy <yusuft.5097@gmail.com>
2024-02-17 19:34:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,27 +8,41 @@ class Library:
         print("Destructor called, Library deleted.")
 
     def listBooks(self):
+        self.lib.seek(0) #move the pointer to the beginning
         txt = self.lib.read().splitlines()
-        for i in txt:
-            print(i)
+        for book in txt:
+            title, author, year, pages = book.strip().split(', ')
+            print(title, author, year, pages)
+        self.lib.seek(0) #reset the pointer
+            
 
     def addBook(self):
-        # self.title = title
-        # self.author = author
-        # self.year = year
-        # self.pages = pages
-
-        metadata = [x for x in input('Add the book information (title, author, year, pages):').split(', ')]
+        metadata = [x for x in input('Add the book information (title, author, year, pages):').strip().split(', ')]
         wcomma = ', '.join(metadata)
         self.lib.write(f"{wcomma}\n")
         print(self.lib)
 
+    def removeBook(self):
+        delBook = input("Type the title of the book you want to delete: ")
+
+        listof = self.lib.read().splitlines()
+
+        for line in listof:
+            new_list = line.split(', ') #assign the current book content to a list
+            print(new_list)
+            if new_list[0] == delBook: #check if the book title matches with the input
+                listof.remove(line) #remove the contents of the book from the list
+                self.lib.truncate(0) #remove all the content in the .txt file starting from the first byte
+                for i in listof: #iterate for every element in the listof
+                    self.lib.write(i+'\n') #write each element and add new line
+                break
 
 
 obj1 = Library()
-obj2 = Library()
-obj1.addBook()
-obj2.addBook()
+#obj1.addBook()
+obj1.listBooks()
+obj1.removeBook()
+
 
 print(obj1)
         
